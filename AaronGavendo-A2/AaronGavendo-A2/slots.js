7a73b87609821d4e8bb540ba858f5ca7978889ca
AaronGavendo <agavendo@hotmail.com>
2014-10-11 18:32:03 -0400

    Add Three Placeholder Images for the Wheel Add buttons to let users bet in 25, 50, 100 Give the user starting chips and have bet buttons interact with it Prevent the user from betting more than they have Add a Button to be used for spinning at a later release

--- AaronGavendo-A2/AaronGavendo-A2/slots.js
+++ AaronGavendo-A2/AaronGavendo-A2/slots.js
@@ -1,1 +1,53 @@
-﻿
+﻿$(document).ready(function () //Function to run when website is loaded
+{
+    var totalAmount = 200;
+    var betAmount;
+    var poorMessage = "You Can't afford this bet!";
+
+    $("#bet25").click(function () //When 25 Bet button is pressed
+    {
+        betAmount = 25;
+        if (totalAmount > 24)
+        {
+            totalAmount = totalAmount - 25;
+            document.getElementById("betAmount").innerHTML = betAmount; //Pass into html (text value)
+            document.getElementById("totalCash").innerHTML = totalAmount; //Pass into html (text value)
+        }
+        else
+        {
+            document.getElementById("betAmount").innerHTML = poorMessage;
+        }
+    });
+
+    $("#bet50").click(function () //When 50 Bet button is pressed
+    {
+        betAmount = 50;
+        if(totalAmount > 49)
+        {
+            totalAmount = totalAmount - 50;
+            document.getElementById("betAmount").innerHTML = betAmount;
+            document.getElementById("totalCash").innerHTML = totalAmount;
+        }
+        else
+        {
+            document.getElementById("betAmount").innerHTML = poorMessage;
+        }
+    });
+
+    $("#bet100").click(function () //When 100 Bet button is pressed
+    {
+        betAmount = 100;
+        if (totalAmount > 99)
+        {
+            totalAmount = totalAmount - 100;
+            document.getElementById("betAmount").innerHTML = betAmount;
+            document.getElementById("totalCash").innerHTML = totalAmount;
+        }
+        else
+        {
+            document.getElementById("betAmount").innerHTML = poorMessage;
+        }
+    });
+
+
+});
